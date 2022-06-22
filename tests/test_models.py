783bae201ae55308d507adf3437e52711bc183bd
mpourostad <mpourostad.91@gmail.com>
2022-06-22 09:07:54 +0000

    cleaning up + developing TestProduct class

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,30 +6,40 @@ import os
 import logging
 import unittest
 from service.models import Product, DataValidationError, db
+from service import app
 
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
 ######################################################################
 #  Product   M O D E L   T E S T   C A S E S
 ######################################################################
-class TestYourResourceModel(unittest.TestCase):
+class TestProduct(unittest.TestCase):
     """ Test Cases for Product Model """
 
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
-        pass
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Product.init_db(app)
+        # pass
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
-        pass
+        db.session.close()
 
     def setUp(self):
         """ This runs before each test """
-        pass
+        db.session.query(Product).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
         """ This runs after each test """
-        pass
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
@@ -38,16 +48,14 @@ class TestYourResourceModel(unittest.TestCase):
     def test_XXXX(self):
         """ It should always be true """
         self.assertTrue(True)
-    def test_create_a_pet(self):
+    def test_create_a_product(self):
         """It should Create a product and assert that it exists"""
-        product = Product(name="Cotton-Hemp Sweater", category="clothes", available=True, description="Relaxed fit sweater with long sleeves")
+        product = Product(name="Cotton-Hemp Sweater", category="sweater", available=True, description="Relaxed fit sweater with long sleeves")
         self.assertEqual(str(product), "<Product 'Cotton-Hemp Sweater' id=[None]>")
         self.assertTrue(product is not None)
         self.assertEqual(product.id, None)
-        self.assertEqual(product.name, "Fido")
-        self.assertEqual(product.category, "dog")
+        self.assertEqual(product.name, "Cotton-Hemp Sweater")
+        self.assertEqual(product.category, "sweater")
         self.assertEqual(product.available, True)
-        self.assertEqual(product.gender, Gender.MALE)
-        product = Product(name="Fido", category="dog", available=False, gender=Gender.FEMALE)
+        product = Product(name="Cotton-Hemp Sweater", category="sweater", available=False, description="Relaxed fit sweater with long sleeves")
         self.assertEqual(product.available, False)
-        self.assertEqual(product.gender, Gender.FEMALE)
