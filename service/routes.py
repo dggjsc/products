b6082c034b03b4907b9999bff2511b9453e0f1ea
mpourostad <mpourostad.91@gmail.com>
2022-06-22 16:39:44 +0000

    changing the name from product to Product REST API Service

--- service/routes.py
+++ service/routes.py
@@ -26,7 +26,7 @@ def index():
     """ Root URL response """
     return (
         # "Reminder: return some useful information in json format about the service here",
-        jsonify( name = "products", paths=url_for("index", _external=True),
+        jsonify( name = "Product REST API Service", paths=url_for("index", _external=True),
         version = "1.0"),
         status.HTTP_200_OK,
     )
