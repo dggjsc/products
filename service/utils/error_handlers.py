e69da35c9acc4f074c56f93417788eea623835a9
Yulu <649783664@qq.com>
2022-06-28 22:56:23 +0000

    comment out 409 in error_handler since 409 is not yet used and increase code coverage by 1%

--- service/utils/error_handlers.py
+++ service/utils/error_handlers.py
@@ -71,19 +71,19 @@ def method_not_supported(error):
     )
 
 
-@app.errorhandler(status.HTTP_409_CONFLICT)
-def resource_conflict(error):
-    """Handles resource conflicts with HTTP_409_CONFLICT"""
-    message = str(error)
-    app.logger.warning(message)
-    return (
-        jsonify(
-            status=status.HTTP_409_CONFLICT,
-            error="Conflict",
-            message=message,
-        ),
-        status.HTTP_409_CONFLICT,
-    )
+# @app.errorhandler(status.HTTP_409_CONFLICT)
+# def resource_conflict(error):
+#     """Handles resource conflicts with HTTP_409_CONFLICT"""
+#     message = str(error)
+#     app.logger.warning(message)
+#     return (
+#         jsonify(
+#             status=status.HTTP_409_CONFLICT,
+#             error="Conflict",
+#             message=message,
+#         ),
+#         status.HTTP_409_CONFLICT,
+#     )
 
 
 @app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
